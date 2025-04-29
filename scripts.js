50a9501153e7daabb68d8cd0a83e23678971f59e
ILuuh <luiz.gouvea2b2@gmail.com>
2025-04-29 12:19:08 -0300

    Atualização estilo e index 29/04

--- scripts.js
+++ scripts.js
@@ -13,3 +13,17 @@ function abrirmenu() {
 function fecharmenu() {
     menu.style.right = "-100%";
 }
+
+// Inclusão do ano dinamicamente
+
+// Capturar o span que receberá a informação
+let spanAno = document.querySelector('#anoAtual');
+
+//Nova instância de um objeto do tipo Data.
+let novadata = new Date();
+
+//Captura da instância o ano de 4 dígitos.
+let ano = novadata.getFullYear();
+
+//Injetar o ano atual no span.
+spanAno.innerHTML = ano;
